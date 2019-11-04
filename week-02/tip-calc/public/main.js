f7c181377c7db47f97e1f8f886f52bea9ea7631b
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-04 11:46:20 -0500

    basic MVP

---
+++ week-02/tip-calc/public/main.js
@@ -0,0 +1,27 @@
+const main = () => {
+  console.log('hello world, this is coming from javascript  on page load')
+}
+
+const calculateTip = () => {
+  console.log('calculating tip')
+  // get the bill total from the input tag
+  const total = document.querySelector('input').value
+  console.log('the total is:' + total)
+  // calculate the differents tips
+  // 15%
+  const fifteenPercentTip = total * 0.15
+  const eighteenPercentTip = total * 0.18
+  const twentyPercentTip = total * 0.2
+
+  console.log(fifteenPercentTip)
+  console.log(eighteenPercentTip)
+  console.log(twentyPercentTip)
+
+  // display the tip amounts
+  document.querySelector('.fifteen-percent').textContent = fifteenPercentTip
+  document.querySelector('.eighteen-percent').textContent = eighteenPercentTip
+  document.querySelector('.twenty-percent').textContent = twentyPercentTip
+}
+
+document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', calculateTip)
