284456176158b5457b0cb7db00cb0126d2373329
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-05 15:31:29 -0500

    with me at 3

--- week-02/number-guesser/public/main.js
+++ week-02/number-guesser/public/main.js
@@ -1,8 +1,13 @@
 // DONE: on page load, generate a random number
-// TODO: when the user clicks the button, we check if the inputted number === generated number
-// TODO: if the user guessed the correct number, display message of success
+// DONE: when the user clicks the button, we check if the inputted number === generated number
+// DONE: if the user guessed the correct number, display message of success
+
+// DONE: Add a counter to count the number of times guessed
+// DONE: limit the number of guesses to 7
+// TODO: Add a message for too high/too low
 
 let randomNumber = 0
+let numberOfGuesses = 0
 
 const main = () => {
   randomNumber = Math.ceil(Math.random() * 10)
@@ -12,18 +17,34 @@ const main = () => {
 const checkTheNumbers = () => {
   // get the number from the HTML input
   const userGuess = parseInt(document.querySelector('input').value)
+  numberOfGuesses++
   console.log(userGuess)
   console.log(typeof userGuess)
   console.log(typeof randomNumber)
   // check if the number === randomNumber
-  if (randomNumber === userGuess) {
+  if (randomNumber === userGuess && numberOfGuesses <= 7) {
     // if yes, show success message
     console.log('success')
-    document.querySelector('.output-message').textContent = 'Success!'
+    document.querySelector('.output-message').textContent =
+      'Success! You guessed the number in ' + numberOfGuesses + ' tries'
+  } else if (numberOfGuesses >= 7) {
+    document.querySelector('.output-message').textContent =
+      'Sorry, you have guessed too many times. Play again?'
+    document.querySelector('button').disabled = true
   } else {
     // if no (else), show try again message
-    console.log('nope')
-    document.querySelector('.output-message').textContent = 'Nope! Try again!'
+    if (randomNumber > userGuess) {
+      document.querySelector('.output-message').classList.add('too-low')
+      document.querySelector('.output-message').classList.remove('too-high')
+
+      document.querySelector('.output-message').textContent =
+        'Try Again with a bigger number!'
+    } else if (randomNumber < userGuess) {
+      document.querySelector('.output-message').textContent =
+        'Try Again with a smaller number!'
+      document.querySelector('.output-message').classList.add('too-high')
+      document.querySelector('.output-message').classList.remove('too-low')
+    }
   }
 }
 
