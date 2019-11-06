68475222f761b2078dceefbcd17c4b97baa5f0e9
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-06 12:00:22 -0500

    added for loop

--- week-02/number-guesser/public/main.js
+++ week-02/number-guesser/public/main.js
@@ -4,46 +4,72 @@
 
 // DONE: Add a counter to count the number of times guessed
 // DONE: limit the number of guesses to 7
-// TODO: Add a message for too high/too low
+// DONE: Add a message for too high/too low
+
+// TODO: Add a message if a user has guessed the number already
 
 let randomNumber = 0
 let numberOfGuesses = 0
+const guessedNumbers = []
 
 const main = () => {
   randomNumber = Math.ceil(Math.random() * 10)
   console.log(randomNumber)
 }
 
+// param: the number checkout
+// return value: true/false if the user has guessed the number
+
+const hasUserGuessedTheNumber = userGuess => {
+  console.log('checking is the user guess ', userGuess)
+  for (let i = 0; i < guessedNumbers.length; i++) {
+    if (guessedNumbers[i] === userGuess) {
+      return true
+    }
+  }
+  return false
+}
+
+const showHighLowMessage = userGuess => {
+  if (randomNumber > userGuess) {
+    document.querySelector('.output-message').classList.add('too-low')
+    document.querySelector('.output-message').classList.remove('too-high')
+
+    document.querySelector('.output-message').textContent =
+      'Try Again with a bigger number!'
+  } else if (randomNumber < userGuess) {
+    document.querySelector('.output-message').textContent =
+      'Try Again with a smaller number!'
+    document.querySelector('.output-message').classList.add('too-high')
+    document.querySelector('.output-message').classList.remove('too-low')
+  }
+}
+
 const checkTheNumbers = () => {
   // get the number from the HTML input
   const userGuess = parseInt(document.querySelector('input').value)
   numberOfGuesses++
-  console.log(userGuess)
-  console.log(typeof userGuess)
-  console.log(typeof randomNumber)
-  // check if the number === randomNumber
-  if (randomNumber === userGuess && numberOfGuesses <= 7) {
-    // if yes, show success message
-    console.log('success')
-    document.querySelector('.output-message').textContent =
-      'Success! You guessed the number in ' + numberOfGuesses + ' tries'
-  } else if (numberOfGuesses >= 7) {
-    document.querySelector('.output-message').textContent =
-      'Sorry, you have guessed too many times. Play again?'
-    document.querySelector('button').disabled = true
-  } else {
-    // if no (else), show try again message
-    if (randomNumber > userGuess) {
-      document.querySelector('.output-message').classList.add('too-low')
-      document.querySelector('.output-message').classList.remove('too-high')
 
+  const hasGuessed = hasUserGuessedTheNumber(userGuess)
+
+  if (hasGuessed) {
+    document.querySelector('.output-message').textContent =
+      'Wait, you have guessed that already!'
+    showHighLowMessage(userGuess)
+  } else {
+    guessedNumbers.push(userGuess)
+    console.log(guessedNumbers)
+    // check if the number === randomNumber
+    if (randomNumber === userGuess && numberOfGuesses <= 7) {
+      // if yes, show success message
       document.querySelector('.output-message').textContent =
-        'Try Again with a bigger number!'
-    } else if (randomNumber < userGuess) {
+        'Success! You guessed the number in ' + numberOfGuesses + ' tries'
+    } else if (numberOfGuesses >= 7) {
       document.querySelector('.output-message').textContent =
-        'Try Again with a smaller number!'
-      document.querySelector('.output-message').classList.add('too-high')
-      document.querySelector('.output-message').classList.remove('too-low')
+        'Sorry, you have guessed too many times. Play again?'
+      document.querySelector('button').disabled = true
+    } else {
+      showHighLowMessage(userGuess)
     }
   }
 }
