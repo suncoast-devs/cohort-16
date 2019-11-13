0d04bed8e3a0e98bc8ca27383350eb5b2bd706f0
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-13 10:32:12 -0500

    updated the prettier and map/foreach example

--- week-02/deck-shuffler/public/main.js
+++ week-02/deck-shuffler/public/main.js
@@ -32,16 +32,28 @@ const getCardValue = rank => {
 }
 
 const main = () => {
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < ranks.length; j++) {
+  // for (let i = 0; i < suits.length; i++) {
+  //   for (let j = 0; j < ranks.length; j++) {
+  //     const card = {
+  //       rank: ranks[j],
+  //       suit: suits[i],
+  //       value: getCardValue(ranks[j])
+  //     }
+  //     deck.push(card)
+  //   }
+  // }
+
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
       const card = {
-        rank: ranks[j],
-        suit: suits[i],
-        value: getCardValue(ranks[j])
+        rank: rank,
+        suit: suit,
+        value: getCardValue(rank)
       }
       deck.push(card)
-    }
-  }
+    })
+  })
+
   for (let i = 0; i < deck.length; i++) {
     const j = Math.floor(Math.random() * 52)
     const temp = deck[i]
@@ -53,11 +65,36 @@ const main = () => {
 
 const dealACard = () => {
   // document.querySelector('.player-hand').textContent = deck[0]
-  const drawnCard = deck.pop()
+  let drawnCard = deck.pop()
   playerHand.push(drawnCard)
-  const cardLi = document.createElement('li')
-  cardLi.textContent = drawnCard.rank + ' of ' + drawnCard.suit
-  document.querySelector('.player-hand').appendChild(cardLi)
+  drawnCard = deck.pop()
+  playerHand.push(drawnCard)
+  drawnCard = deck.pop()
+  playerHand.push(drawnCard)
+
+  // loop over the user hand, and create an li for each item and append to the dom
+  document.querySelector('.player-hand').textContent = ''
+  // while (document.querySelector('.player-hand').childNodes.length > 0) {
+  //   document.querySelector('.player-hand').removeChild(document.querySelector('.player-hand').firstChild)
+  // }
+
+  // for (let i = 0; i < playerHand.length; i++) {
+  //   const card = playerHand[i]
+  //   const cardLi = document.createElement('li')
+  //   cardLi.textContent = card.rank + ' of ' + card.suit
+  //   document.querySelector('.player-hand').appendChild(cardLi)
+  // }
+
+  const listItems = playerHand.map(card => {
+    const cardLi = document.createElement('li')
+    cardLi.textContent = card.rank + ' of ' + card.suit
+    return cardLi
+  })
+
+  console.log(listItems)
+  listItems.forEach(listItem => {
+    document.querySelector('.player-hand').appendChild(listItem)
+  })
 
   let sum = 0
   for (let i = 0; i < playerHand.length; i++) {
