0320727226a0a551c0225c96946674d98dd53d2f
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-18 11:41:49 -0500

    first react app

--- week-04/star-wars-fan-page/src/App.js
+++ week-04/star-wars-fan-page/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
-  }
+import obiWanPicture from './images/obi-wan.jpeg'
+import ackarPicture from './images/ackbar.jpeg'
+import chewie from './images/chewie.jpeg'
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>I love Star Wars!</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <img src={obiWanPicture} />
+            <h2>Obi-wan</h2>
+            <p>Use the force!</p>
+          </li>
+          <li>
+            <img src={chewie} />
+            <h2>Chewie</h2>
+            <p>Raaarrrrrr rarrrr!</p>
+          </li>
+          <li>
+            <img src={ackarPicture} />
+            <h2>Ackbar</h2>
+            <p>It's a trap!</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  )
 }
 
 export default App
