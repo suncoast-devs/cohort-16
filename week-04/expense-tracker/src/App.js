f37d717038adb1a634c768562eda6770263ece91
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-20 12:14:14 -0500

    refactored the expenseitem

--- week-04/expense-tracker/src/App.js
+++ week-04/expense-tracker/src/App.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 import axios from 'axios'
+import ExpenseItem from './components/ExpenseItem'
 
 const App = () => {
   const [expenses, setExpenses] = useState([])
+
+  const getDataFromAPI = async () => {
+    // const resp = await axios.get(
+    //   'https://sdg-expense-api.herokuapp.com/api/expense'
+    // )
+    // console.log(resp.data)
+
+    const resp = await fetch(
+      'https://sdg-expense-api.herokuapp.com/api/expense'
+    )
+    const data = await resp.json()
+    console.log(data)
+    setExpenses(data)
+  }
+
+  useEffect(() => {
+    console.log('using the effect')
+    // call my API
+    getDataFromAPI()
+  }, [])
 
   return (
     <>
       Expense Tracker
       <ul>
         {expenses.map(expense => {
-          return <li>{expense.note}</li>
+          return (
+            <ExpenseItem
+              key={expense.id}
+              note={expense.note}
+              type={expense.type}
+              when={expense.when}
+              amount={expense.amount}
+            />
+          )
         })}
       </ul>
     </>
