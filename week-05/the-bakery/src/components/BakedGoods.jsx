efb750292c498b00ecfa4fea7d477932535894d8
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-25 12:04:17 -0500

    added json data

--- week-05/the-bakery/src/components/BakedGoods.jsx
+++ week-05/the-bakery/src/components/BakedGoods.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import data from './data/baked-goods.json'
+
 const BakedGoods = props => {
+  // filter down the data object to only find this categories food
+  const food = data.categories.filter(item => {
+    return item.category === props.match.params.category
+  })[0]
+  console.log(food)
   return (
-    <div>
-      <h1>Hello, you are on the {props.match.params.category} page!!!</h1>
+    <div className={props.match.params.category}>
+      <h1>Hello, you are on the {food.category} page!!!</h1>
+      <ul>
+        {food.treats.map(treat => {
+          return (
+            <li>
+              <header>{treat.title}</header>
+              <img src={treat.image} alt="" />
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
