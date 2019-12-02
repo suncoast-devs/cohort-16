480d3edc3a0336fd45b8460d1f3be4ca1ff49f9c
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-02 10:57:30 -0500

    ready for game logic, added random word

--- week-06/snowman/src/pages/HomePage.jsx
+++ week-06/snowman/src/pages/HomePage.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import words from '../data/words.json'
 
 const HomePage = () => {
-  return <HelloWorld />
+  console.log(words)
+
+  const [selectedWord, setSelectedWord] = useState(
+    words[Math.floor(Math.random() * words.length)]
+  )
+
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const letterClicked = letter => {
+    console.log(letter)
+  }
+
+  return (
+    <main>
+      <img src="" alt="snowman" />
+      <section>
+        <ul>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+        </ul>
+      </section>
+      <section>
+        <header>guessed letters: </header>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+          <li>d</li>
+        </ul>
+      </section>
+      <section>
+        <ul className="letter-buttons">
+          {letters.map(letter => {
+            return (
+              <li key={letter}>
+                <button onClick={() => letterClicked(letter)}>
+                  {letter.toUpperCase()}
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </main>
+  )
 }
 
 export default HomePage
