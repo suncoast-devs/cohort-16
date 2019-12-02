2b1068731de12902c1b610b9dd5372d36063c660
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-02 11:49:19 -0500

    added  snowman picture

--- week-06/snowman/src/pages/HomePage.jsx
+++ week-06/snowman/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import words from '../data/words.json'
+
+import step0 from '../images/step_0.png'
+import step1 from '../images/step_1.png'
+import step2 from '../images/step_2.png'
+import step3 from '../images/step_3.png'
+import step4 from '../images/step_4.png'
+import step5 from '../images/step_5.png'
+import step6 from '../images/step_6.png'
+import step7 from '../images/step_7.png'
+
+const images = [step0, step1, step2, step3, step4, step5, step6, step7]
 
 const HomePage = () => {
   const [selectedWord, setSelectedWord] = useState('apple')
   // words[Math.floor(Math.random() * words.length)]
 
   const [strikes, setStrikes] = useState(7)
-
   const [lettersGuessed, setLettersGuessed] = useState([])
+  const [gameStatus, setGameStatus] = useState('playing')
 
   const letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
   const letterClicked = letter => {
@@ -25,20 +36,29 @@ const HomePage = () => {
     }
   }
 
+  useEffect(() => {
+    if (strikes === 0) {
+      setGameStatus('lost')
+    }
+  }, [strikes])
+
   return (
     <main>
       <section>{strikes}</section>
-      <img src="" alt="snowman" />
+      <img src={images[strikes]} alt="snowman" />
       <section>
         <ul>
           {selectedWord.split('').map((letter, index) => {
-            if (lettersGuessed.includes(letter)) {
+            if (lettersGuessed.includes(letter) || gameStatus === 'lost') {
               return <li key={index}>{letter}</li>
             } else {
               return <li key={index}>_</li>
             }
           })}
         </ul>
+        {gameStatus === 'lost' && (
+          <section>The word was {selectedWord}</section>
+        )}
       </section>
       <section>
         <header>guessed letters: </header>
