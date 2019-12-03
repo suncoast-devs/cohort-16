61629af2a1eaca0a7d33a6789980d979e3824d6b
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-03 09:55:34 -0500

    added missing buttons

--- week-06/calculator-review/src/components/Calculator.jsx
+++ week-06/calculator-review/src/components/Calculator.jsx
@@ -9,25 +9,29 @@ const Calculator = () => {
       <section className="button-row">
         <button className="clear-button">clear</button>
         <button className="operator-button">/</button>
-        <button className="operator-button">*</button>
       </section>
       <section className="button-row">
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button className="operator-button">-</button>
+        <button className="operator-button">*</button>
       </section>
       <section className="button-row">
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button className="operator-button">+</button>
+        <button className="operator-button">-</button>
       </section>
       <section className="button-row">
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button className="operator-button">=</button>
+        <button className="operator-button">+</button>
+      </section>
+      <section className="button-row">
+        <button className="zero">0</button>
+
+        <button className="operator-button equals">=</button>
       </section>
     </main>
   )
