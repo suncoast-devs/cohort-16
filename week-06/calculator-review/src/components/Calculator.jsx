cfee93139490b968e04efe5cf7ea40b855bcc4a0
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-03 09:34:20 -0500

    styled

---
+++ week-06/calculator-review/src/components/Calculator.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <main className="calculator">
+      <section className="display">
+        <p>display</p>
+      </section>
+      <section className="button-row">
+        <button className="clear-button">clear</button>
+        <button className="operator-button">/</button>
+        <button className="operator-button">*</button>
+      </section>
+      <section className="button-row">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="operator-button">-</button>
+      </section>
+      <section className="button-row">
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="operator-button">+</button>
+      </section>
+      <section className="button-row">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="operator-button">=</button>
+      </section>
+    </main>
+  )
+}
+
+export default Calculator
--- week-06/calculator-review/src/pages/HomePage.jsx
+++ week-06/calculator-review/src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import Calculator from '../components/Calculator'
 
 const HomePage = () => {
-  return <HelloWorld />
+  return (
+    <main className="container">
+      <Calculator />
+    </main>
+  )
 }
 
 export default HomePage
