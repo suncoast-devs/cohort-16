00920a4c254898242112371d7928fc08c3e97154
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-05 12:57:43 -0500

    added ts example

--- week-06/ts-example/src/components/TipCalculator.tsx
+++ week-06/ts-example/src/components/TipCalculator.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 
 const TipCalculator = () => {
+  const [billTotal, setBillTotal] = useState<number | string>(0)
+  const [tipAmount, setTipAmount] = useState<number>(0)
+
+  useEffect(() => {
+    const tip = parseFloat(billTotal as any) * 0.18
+    setTipAmount(tip)
+  }, [billTotal])
+
   return (
     <div>
-      
+      <input
+        type="number"
+        placeholder="Bill total"
+        value={billTotal}
+        onChange={e => setBillTotal(e.target.value)}
+      />
+      <p>18% tip is ???</p>
+      <p>Total amount is ???</p>
     </div>
-  );
+  )
 }
 
-export default TipCalculator;
+export default TipCalculator
