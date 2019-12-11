bd0e099aabee4a9515f99820506447a910a57b7b
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-11 13:45:44 -0500

    tips

---
+++ week-07/TipCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TipCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What is the bill total?");
+      var billTotal = Console.ReadLine();
+      Console.WriteLine($"Your bill was {billTotal}");
+      var grandTotal =
+        decimal.Parse(billTotal) +
+        (decimal.Parse(billTotal) * (decimal)0.20);
+
+      Console.WriteLine($"The grand total with tip is {grandTotal}");
+
+    }
+  }
+}
