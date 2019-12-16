189a089bc11a87a127c9cfe0201ef0712e2f4b33
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-16 11:29:37 -0500

    set up for database !

--- week-07/CryptidTracker/Cryptid.cs
+++ week-07/CryptidTracker/Cryptid.cs
@@ -4,6 +4,8 @@ namespace CryptidTracker
 {
   public class Cryptid
   {
+
+    public int Id { get; set; }
     // Name
     public string Name { get; set; }
     // NumberOfSightings
---
+++ week-07/CryptidTracker/Migrations/20191216161948_AddedFirstTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CryptidTracker.Migrations
+{
+    public partial class AddedFirstTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cryptids",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    NumberOfSightings = table.Column<int>(nullable: false),
+                    LastSightedAt = table.Column<string>(nullable: true),
+                    DateOfLastSighting = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cryptids", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cryptids");
+        }
+    }
+}
