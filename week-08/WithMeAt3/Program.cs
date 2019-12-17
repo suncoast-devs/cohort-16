1a1f962a8fe9348442755e36691e146bba9db69e
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-17 16:16:39 -0500

    added with me at 3

---
+++ week-08/WithMeAt3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WithMeAt3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      var str = "dog";
+      Console.ReadKey();
+      var successful = int.TryParse(str, out int banana);
+      if (successful)
+      {
+        Console.WriteLine(":Good work, your number is" + banana);
+      }
+      else
+      {
+        Console.WriteLine("Try again with a numbert");
+      }
+
+
+
+    }
+  }
+}
