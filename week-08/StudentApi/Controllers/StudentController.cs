4059db66ccf854103bb96edc32e9eb116ae4166d
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-18 12:08:00 -0500

    added  PUT and DELETE

--- week-08/StudentApi/Controllers/StudentController.cs
+++ week-08/StudentApi/Controllers/StudentController.cs
@@ -17,6 +17,21 @@ namespace StudentApi.Controllers
       return Ok(db.Students.OrderBy(student => student.FullName));
     }
 
+    [HttpGet("{id}")]
+    public ActionResult GetOneStudent(int id)
+    {
+      var db = new DatabaseContext();
+      var student = db.Students.FirstOrDefault(st => st.Id == id);
+      if (student == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(student);
+      }
+    }
+
     [HttpPost]
     public ActionResult CreateStudent(Student student)
     {
@@ -24,6 +39,42 @@ namespace StudentApi.Controllers
       db.Students.Add(student);
       db.SaveChanges();
       return Ok(student);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateStudent(Student student)
+    {
+      var db = new DatabaseContext();
+      var prevStudent = db.Students.FirstOrDefault(st => st.Id == student.Id);
+      if (prevStudent == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        prevStudent.FullName = student.FullName;
+        prevStudent.GPA = student.GPA;
+        prevStudent.IsJoyful = student.IsJoyful;
+        db.SaveChanges();
+        return Ok(prevStudent);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteStudent(int id)
+    {
+      var db = new DatabaseContext();
+      var student = db.Students.FirstOrDefault(st => st.Id == id);
+      if (student == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        db.Students.Remove(student);
+        db.SaveChanges();
+        return Ok();
+      }
     }
 
   }
