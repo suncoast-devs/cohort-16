45452ec18b07c23dbe60eb7dd65fd9528a4fef68
Mark Dewey <mark.t.dewey@gmail.com>
2020-01-07 11:31:08 -0500

    basic search

---
+++ week-10/HikingTrailApi/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HikingTrailApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HikingTrailApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController : ControllerBase
+  {
+    private readonly DatabaseContext db;
+
+    public SearchController(DatabaseContext context)
+    {
+      this.db = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> SearchParks([FromQuery]string searchTerm)
+    {
+
+      var results = db.Parks
+        .Where(park =>
+            park.Name.ToLower().Contains(searchTerm.ToLower()) ||
+            park.City.ToLower().Contains(searchTerm.ToLower()) ||
+            park.Zip.Contains(searchTerm) ||
+            park.Address.ToLower().Contains(searchTerm.ToLower())
+        );
+      return Ok(results);
+    }
+  }
+}
